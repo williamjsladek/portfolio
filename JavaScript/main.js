12f8c1d25304b62a9bcef7d959e0b5965de6b01f
williamjsladek <74747276+williamjsladek@users.noreply.github.com>
2023-03-15 18:56:39 -0300

    adding the page and content template

--- JavaScript/main.js
+++ JavaScript/main.js
@@ -5,10 +5,6 @@ var menuIcon = document.getElementById("menu-icon");
 menuIcon.addEventListener("click", togglehiddenNav);
 
 function togglehiddenNav() {
-    // console.log("test");
     nav.classList.toggle("collapsed");
-    // nav.classList.toggle("cToGrey-bcToJet");
     menuIcon.classList.toggle("menu-icon-toggle");
-    // menuIcon.classList.toggle("cToGrey-bcToJet");
-    // navContent.classList.toggle("hidden");
 }
